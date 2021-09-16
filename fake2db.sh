1769a464fd07095d2e33bce641f1ca5dedde06fa
malinka <70282392+malinkaphann@users.noreply.github.com>
2021-09-16 21:02:22 +0700

    update

--- fake2db.sh
+++ fake2db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-fake2db --db postgresql --rows 1000000 --host localhost --user $PG_USER --password $PG_PASS --name ${PG_DB} --custom name email password address country
+fake2db --db postgresql --rows 1000000 --host $PG_HOST --user $PG_USER --password $PG_PASS --name $PG_DB --custom name email password address country
